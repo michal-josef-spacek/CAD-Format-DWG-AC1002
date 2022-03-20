896e9307c8b1017a0f095fa0911d5160e24d33d2
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-03-20 16:55:56 +0100

    Fix current_color_convert and unknown after it

--- AC1002.pm
+++ AC1002.pm
@@ -2703,11 +2703,11 @@ sub _read {
     $self->{text_size} = $self->{_io}->read_f8le();
     $self->{trace_width} = $self->{_io}->read_f8le();
     $self->{current_layer_index} = $self->{_io}->read_s2le();
-    $self->{current_color_convert} = $self->{_io}->read_f8le();
-    $self->{unknown7a} = $self->{_io}->read_bytes(2);
-    $self->{unknown7b} = $self->{_io}->read_bytes(2);
-    $self->{unknown7c} = $self->{_io}->read_bytes(2);
-    $self->{unknown7d} = $self->{_io}->read_bytes(2);
+    $self->{current_color_convert} = $self->{_io}->read_s2le();
+    $self->{unknown6} = $self->{_io}->read_f8le();
+    $self->{unknown7a} = $self->{_io}->read_s2le();
+    $self->{unknown7b} = $self->{_io}->read_s2le();
+    $self->{unknown7c} = $self->{_io}->read_s2le();
     $self->{unknown8} = $self->{_io}->read_f8le();
     $self->{linear_units_format} = $self->{_io}->read_s2le();
     $self->{linear_units_precision} = $self->{_io}->read_s2le();
@@ -2950,6 +2950,11 @@ sub current_color_convert {
     return $self->{current_color_convert};
 }
 
+sub unknown6 {
+    my ($self) = @_;
+    return $self->{unknown6};
+}
+
 sub unknown7a {
     my ($self) = @_;
     return $self->{unknown7a};
@@ -2963,11 +2968,6 @@ sub unknown7b {
 sub unknown7c {
     my ($self) = @_;
     return $self->{unknown7c};
-}
-
-sub unknown7d {
-    my ($self) = @_;
-    return $self->{unknown7d};
 }
 
 sub unknown8 {
