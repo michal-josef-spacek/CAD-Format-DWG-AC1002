36a55f9ee7cb2970984dbf60b0802b3d9e5389a1
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-05-28 13:41:23 +0200

    Rewrite circle entity to center_point

--- AC1002.pm
+++ AC1002.pm
@@ -3994,8 +3994,7 @@ sub _read {
     my ($self) = @_;
 
     $self->{entity_common} = CAD::Format::DWG::AC1002::EntityCommon->new($self->{_io}, $self, $self->{_root});
-    $self->{x} = $self->{_io}->read_f8le();
-    $self->{y} = $self->{_io}->read_f8le();
+    $self->{center_point} = CAD::Format::DWG::AC1002::Point2d->new($self->{_io}, $self, $self->{_root});
     $self->{radius} = $self->{_io}->read_f8le();
 }
 
@@ -4004,14 +4003,9 @@ sub entity_common {
     return $self->{entity_common};
 }
 
-sub x {
+sub center_point {
     my ($self) = @_;
-    return $self->{x};
-}
-
-sub y {
-    my ($self) = @_;
-    return $self->{y};
+    return $self->{center_point};
 }
 
 sub radius {
